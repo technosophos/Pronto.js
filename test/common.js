5517456b49307878aabf784a700707af261e6ab6
Matt Butcher <technosophos@gmail.com>
2011-10-03 16:38:54 -0500

    Fixed registry tests.

---
+++ test/common.js
@@ -0,0 +1,17 @@
+util = require('util');
+pronto = require('../lib/pronto');
+exports.TestCommand = TestCommand;
+
+
+// FIXTURE: Simple test command.
+function TestCommand() {
+	
+}
+util.inherits(TestCommand, pronto.Command);
+
+TestCommand.prototype.execute = function(cxt, params) {
+	this.cxt = cxt;
+	this.params = params;
+	
+	this.done();
+}
--- test/test-configuration.js
+++ test/test-configuration.js
@@ -2,6 +2,25 @@ var common = require('./common');
 var pronto = require('../lib/pronto.js');
 var assert = require('assert');
 
-pronto.declare.request('foo');
+var c = pronto.register.config;
 
-assert.ok(pronto.configuration.config.requests['foo'] != undefined, 'Request should have been created.');
+// Register just a request. CANARY. Remove this, as it is testing an invalid state.
+pronto.register.request('foo');
+assert.ok(c.requests['foo'] != undefined, 'Request should have been created.');
+
+
+// Register one command
+pronto.register.request('bar').doesCommand('test-command').whichInvokes(common.TestCommand);
+assert.ok(pronto.register.config.requests['bar'] != undefined, 'Request should have been created.');
+assert.equal(1, pronto.register.config.requests.bar.length, 'Request should have one command.');
+
+// Register two commands
+pronto.register.request('bar')
+  .doesCommand('test-command').whichInvokes(common.TestCommand)
+	.doesCommand('test-command2').whichInvokes(common.TestCommand)
+;
+
+assert.ok(pronto.register.config.requests['bar'] != undefined, 'Request should have been created.');
+assert.equal(2, pronto.register.config.requests.bar.length, 'Request should have one command.');
+assert.equal('test-command2', c.requests.bar[1].name, 'Command name should be properly set.');
+assert.equal(common.TestCommand, c.requests.bar[0].command, 'Check that command is correct.');

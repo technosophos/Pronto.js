49260b57afa2198f81a9b49c3c777e7aff68076a
Matt Butcher <technosophos@gmail.com>
2011-10-03 17:18:19 -0500

    Fixed several bugs in registry.

--- Makefile
+++ Makefile
@@ -1,9 +1,6 @@
 SHELL := /bin/bash
 
 test:
-	@find test/test-*.js -exec node {} +
-
-oldtest:
 	@find test/test-*.js | xargs -n 1 -t node
 
 .PHONY: test
--- test/test-registry.js
+++ test/test-registry.js
@@ -3,11 +3,10 @@ var pronto = require('../lib/pronto.js');
 var assert = require('assert');
 
 var c = pronto.register.config;
-
+console.log('running...');
 // Register just a request. CANARY. Remove this, as it is testing an invalid state.
 pronto.register.request('foo');
 assert.ok(c.requests['foo'] != undefined, 'Request should have been created.');
-
 
 // Register one command
 pronto.register.request('bar').doesCommand('test-command').whichInvokes(common.TestCommand);
@@ -28,13 +27,22 @@ assert.equal(common.TestCommand, c.requests.bar[0].command, 'Check that command 
 // Test setting values for params.
 pronto.register.request('withArgs')
 	.doesCommand('test-command').whichInvokes(common.TestCommand)
-		.withParam('myParam').withValue('test value')
+		.usingParam('myParam').withValue('test value')
 	.doesCommand('test-command2').whichInvokes(common.TestCommand)
-		.withParam('myParam').withDefault('test value2')
+		.usingParam('myParam').withDefault('test value2')
 	.doesCommand('test-command3').whichInvokes(common.TestCommand)
-		.withParam('myParam').from('cxt:test-command')
+		.usingParam('myParam').from('cxt:test-command')
 ;
 
-assert.equal('test value', c.requests.withArgs[0].params.myParam, 'Test that default value is set.');
-assert.equal('test value2', c.requests.withArgs[1].params.myParam, 'Test that default value is set.');
-assert.equal('cxt:test-command', c.requests.withArgs[2].from, 'Test that from is set.');
+assert.equal('test value', c.requests.withArgs[0].params.myParam.value, 'Test that default value is set.');
+assert.equal('test value2', c.requests.withArgs[1].params.myParam.value, 'Test that default value is set.');
+assert.equal('cxt:test-command', c.requests.withArgs[2].params.myParam.from, 'Test that from is set.');
+
+// Also test that the getRequestSpec() function works:
+var spec = pronto.register.getRequestSpec('withArgs');
+assert.ok(spec, 'Spec should exist.');
+assert.equal('test value', spec[0].params.myParam.value, 'Test that default value is set.');
+
+// Check that an Error is thrown when a nonexistenc request spec is fetched.
+
+assert.throws(function() {pronto.registry.getRequestSpec('NoSuchRequestName');console.log('foo')}, '/No Sflurp request/', 'Should throw an exception if no requst is found.')

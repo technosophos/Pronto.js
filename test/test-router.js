14b307ad9c82afb7c8e0e2103cbca5436a8dbd77
Matt Butcher <technosophos@gmail.com>
2011-10-04 10:10:33 -0500

    Router executes certain events, now.
    Tests updated accordingly.

--- test/test-router.js
+++ test/test-router.js
@@ -18,3 +18,22 @@ var router = new pronto.Router();
 // Test the resolver. Normally, this is done internally.
 var rname = router.resolveRequest('foo');
 assert.equal('foo', rname, 'Request name should be "foo".');
+
+var execWasFired = false;
+router.on('exec', function (spec, context) {
+	assert.ok(context instanceof pronto.Context);
+	//assert.ok(spec instanceof StinkyCheese);
+	execWasFired = true;
+});
+
+var doneWasFired = false;
+router.on('done', function (context) {
+	assert.ok(context instanceof pronto.Context);
+	
+	doneWasFired = true;
+});
+
+router.handleRequest('foo');
+
+assert.ok(execWasFired, '"exec" event must be executed.');
+assert.ok(doneWasFired, '"done" event must be executed.');

a143e13e4f7bfa62dfb7ba9ed243d12fea9e843b
Matt Butcher <technosophos@gmail.com>
2011-10-07 16:00:56 -0500

    Added package.json
    Also added a test script that is unfinished, but a good example.

---
+++ test/xtest-server.js
@@ -0,0 +1,27 @@
+var pronto = require('../lib/pronto');
+var FAIL = require('./common').FailingCommand;
+
+var register = new pronto.Registry();
+
+register
+
+.request('/test')
+	.does('../lib/commands/HTTPResponse', 'out')
+	.using('body', 'This is a test.')
+
+.request('/fail')
+	.does(FAIL, 'out')
+	.using('body', 'This is a test.')
+	
+.request('@404')
+	.does('../lib/commands/HTTPResponse', 'out')
+	.using('code', 404)
+	.using('contentType', 'text/markdown')
+	.using('body', 'Bork bork bork.')
+	
+;
+	
+var cxt = new pronto.Context();
+cxt.add('base-item', 'test');
+
+pronto.HTTPServer.createServer(register, cxt).listen(8000, 'localhost');

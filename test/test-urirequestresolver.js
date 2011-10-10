04e7c5ce19494592a5cb9b55a14107291029b81c
Matt Butcher <technosophos@gmail.com>
2011-10-10 15:35:30 -0500

    Request resolver for URIs is ready.

--- test/test-urirequestresolver.js
+++ test/test-urirequestresolver.js
@@ -6,15 +6,31 @@ var assert = require('assert');
 var register = new Registry();
 
 register
-	.request('a').does(common.TestCommand)
-	.request('a/b').does(common.TestCommand)
-	.request('a/b/c').does(common.TestCommand)
-	.request('a/b/*').does(common.TestCommand)
-	.request('a/b/a').does(common.TestCommand)
+	.request('/a').does(common.TestCommand)
+	.request('/a/b').does(common.TestCommand)
+	.request('/a/b/c').does(common.TestCommand)
+	.request('/a/b/*').does(common.TestCommand)
+	.request('/a/b/*/d/e/f/g/h').does(common.TestCommand)
+	.request('/a/b/a').does(common.TestCommand)
+	.request('/a/c').does(common.TestCommand)
+	.request('@internal/foo').does(common.TestCommand)
 	.request('a/c').does(common.TestCommand)
 ;
 
 var rez = new Resolver();
 rez.buildTree(register);
 
-console.log(rez.tree);
+assert.equal('/a', rez.resolve('/a'));
+assert.equal('/a/b', rez.resolve('/a/b'));
+assert.equal('/a/b/c', rez.resolve('/a/b/c'));
+assert.equal('/a/b/*', rez.resolve('/a/b/WILDCARD'));
+assert.equal('/a/b/*/d/e/f/g/h', rez.resolve('/a/b/WILDCARD/d/e/f/g/h'));
+assert.equal('/a/b/*/d/e/f/g/h', rez.resolve('/a/b/Old macdonald had  a farm/d/e/f/g/h'));
+assert.equal('/a/b/a', rez.resolve('/a/b/a'));
+assert.equal('/a/c', rez.resolve('/a/c'));
+assert.equal('@internal/foo', rez.resolve('@internal/foo'));
+assert.equal('a/c', rez.resolve('a/c'));
+
+assert.equal(undefined, rez.resolve('a/b/c'));
+assert.equal(undefined, rez.resolve('/b/b/c'));
+assert.equal(undefined, rez.resolve('/a/b/a/b'));

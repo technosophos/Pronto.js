0769bba482f3b540dcd179754fa1846fdb4c5842
Matt Butcher <technosophos@gmail.com>
2011-10-13 16:14:13 -0500

    Added JSONEncode test, fixed JSONEncode bug.

---
+++ test/test-jsonencode.js
@@ -0,0 +1,21 @@
+var assert = require('assert');
+var pronto = require('../lib/pronto');
+var JSONEncode = require('../lib/commands/jsonencode');
+
+var reg = new pronto.Registry();
+var cxt = new pronto.Context();
+var objToEncode = {'foo': 'bar', 'hello': 'world'};
+cxt.add('encodeMeBaby', objToEncode);
+
+reg
+  .request('jsonTest')
+	  .does(JSONEncode, 'encoded').using('data').from('cxt:encodeMeBaby')
+;
+
+runner = new pronto.Router(reg);
+runner.once('commandListComplete', function(cxt) {
+	var str = cxt.get('encoded')
+	assert.ok(typeof str == 'string');
+	assert.equal(JSON.stringify(objToEncode), str);
+});
+runner.handleRequest('jsonTest', cxt);
